ab7a13108977f80d274e9ce580ea07ac67ce2228
dzmitryn <dzmitrysoft@gmail.com>
2020-07-19 20:46:50 +0300

    build Angular 10

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule, MatToolbarModule } from '@angular/material';
 
 import { AppRoutingModule } from './app-routing';
 import { AppComponent } from './app.component';
